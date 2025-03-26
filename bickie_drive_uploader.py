3efab350d187e868057d89ceac59818c07227475
BickieAI <git@heromods.com>
2025-03-26 11:29:34 +0800

    Add files via upload

--- bickie_drive_uploader.py
+++ bickie_drive_uploader.py
@@ -84,4 +84,4 @@ def credentials_to_dict(creds):
     }
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
